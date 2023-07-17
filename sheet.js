a4757dfd7136acf6e185a7d4f780e693e7f88098
Asylumreid <wonderlandspectrum@gmail.com>
2023-07-17 16:51:32 +0800

    update and create featrure

--- sheet.js
+++ sheet.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import { sheets, drive } from './GooglesheetsAPI/auth.js';
+import { google } from 'googleapis';
+import credentials from '../GooglesheetsAPI/secrets.json' assert { type: 'json' };
+
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
+});
+
+export const sheets = google.sheets({ version: 'v4', auth });
+export const drive = google.drive({ version: 'v3', auth });
 
 const app = express();
 app.use(express.json());
 
 app.post('/create-spreadsheet', async (req, res) => {
-  try {
+   try {
     const { eventTitle, columnHeaders } = req.body;
 
     const sheets = google.sheets({ version: 'v4', auth });
@@ -55,6 +64,46 @@ app.post('/create-spreadsheet', async (req, res) => {
     });
   }
 });
+
+app.post('/update-spreadsheet', async (req, res) => {
+  try {
+    const { spreadsheetId, data } = req.body;
+
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    // Get the current data in the spreadsheet
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: 'Sheet1',
+    });
+    const values = response.data.values;
+
+    // Determine the next empty row
+    const nextRow = values ? values.length + 1 : 2; // Assuming the data starts from the second row (excluding headers)
+
+    // Update the data in the spreadsheet
+    const updateResponse = await sheets.spreadsheets.values.update({
+      spreadsheetId,
+      range: `Sheet1!A${nextRow}`,
+      valueInputOption: 'RAW',
+      requestBody: {
+        values: [data],
+      },
+    });
+
+    console.log('Spreadsheet data updated:', updateResponse.data);
+
+    res.status(200).json({
+      message: 'Spreadsheet data updated successfully.',
+    });
+  } catch (error) {
+    console.error('Error updating spreadsheet data:', error);
+    res.status(500).json({
+      error: 'An error occurred while updating the spreadsheet data.',
+    });
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000.');
